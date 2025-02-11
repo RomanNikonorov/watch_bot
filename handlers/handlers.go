6d83b29a947fd70092243845617d9fb584ba500a
Roman Nikonorov <roman@rnikonorov.ru>
2025-02-11 16:23:15 +0700

    graylog, graceful & metrics

---
+++ handlers/handlers.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+func Healthz(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func Readyz(isReady *atomic.Value) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		if isReady == nil || !isReady.Load().(bool) {
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"context"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 	"watch_bot/bots"
 	"watch_bot/watch"
+
+	"watch_bot/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/Graylog2/go-gelf.v1/gelf"
 )
 
 func main() {
+	graylogAddr := os.Getenv("GRAYLOG_ADDR")
+	// greylog
+	if graylogAddr != "" {
+		gelfWriter, err := gelf.NewWriter(graylogAddr)
+		if err != nil {
+			log.Fatalf("gelf.NewWriter: %s", err)
+		}
+		// log to both stderr and graylog
+		log.SetOutput(io.MultiWriter(os.Stderr, gelfWriter))
+		log.Printf("logging to stderr & graylog@'%s'", graylogAddr)
+	}
 
 	botToken := os.Getenv("BOT_TOKEN")
 	botApiUrl := os.Getenv("BOT_API_URL")
@@ -58,6 +83,38 @@ func main() {
 		}
 		go watch.Dog(config)
 	}
+
+	// graceful shutdown
+	_, cancel := context.WithCancel(context.Background())
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		s := <-sigCh
+		log.Printf("got signal %v, attempting graceful shutdown", s)
+		cancel()
+		wg.Done()
+	}()
+
+	// metrics & probes
+	isReady := &atomic.Value{}
+	isReady.Store(true)
+	httpRouter := chi.NewRouter()
+	httpRouter.Use(middleware.RequestID)
+	httpRouter.Use(middleware.Logger)
+	httpRouter.Use(middleware.Recoverer)
+
+	httpRouter.HandleFunc("/health", handlers.Healthz)
+	httpRouter.HandleFunc("/ready", handlers.Readyz(isReady))
+	httpRouter.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		err := http.ListenAndServe(":9000", httpRouter)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	for {
 		time.Sleep(time.Duration(probeDelay) * time.Second)
