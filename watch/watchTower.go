b9aa48bd683d1e6df3a900ee1e5dceab8530355b
Roman Nikonorov <roman@rnikonorov.ru>
2025-03-05 11:19:03 +0700

    minor update

--- watch/watchTower.go
+++ watch/watchTower.go
@@ -80,7 +80,7 @@ func waitForWakeUp(config DogConfig, waitChan chan bool, client HTTPClient) {
 		log.Printf("Server %s is still dead after %d probes", config.Server.Name, i+1)
 	}
 	pauseMinutes := config.DeadPause
-	config.MessagesChannel <- bots.Message{ChatId: config.ChatId, Text: "❌❌❌ " + config.Server.Name + " is offline, pause watching it for " + strconv.Itoa(pauseMinutes) + " minutes ❌❌❌"}
+	config.MessagesChannel <- bots.Message{ChatId: config.ChatId, Text: "☠️ " + config.Server.Name + " is offline, pause watching it for " + strconv.Itoa(pauseMinutes) + " minutes ☠️"}
 	time.Sleep(time.Duration(pauseMinutes) * time.Minute)
 	checkAndReport(config, client, waitChan)
 }
